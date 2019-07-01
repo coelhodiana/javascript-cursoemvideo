17a11abb7da9bf140f0753d68dbc8a523e837f8a
@coelhodiana <coelhodiana04@gmail.com>
2019-07-01 20:42:16 -0300

    Commit Inicial

---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+
+//condição composta
+if ( país == 'Brasil' ){
+    console.log('Brasileiro!')
+} else {
+    console.log('Você é brasileiro.')
+}
+
